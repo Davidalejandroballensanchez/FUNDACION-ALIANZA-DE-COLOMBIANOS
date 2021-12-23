80aabcb40190792e44ea41924fd4fb6f06883c9e
David Alejandro Ballen Sanchez <aballenplays@gmail.com>
2021-12-23 17:20:34 -0500

    En este commit agregamos la hoja de script llamada 'main.js'. En ella agregamos unas funciones para que la pagina mande unos mensajes a los usuarios cuando ellso precionen cierto botones. XDXDXD

---
+++ js personalizado/main.js
@@ -0,0 +1,17 @@
+// FUNCIONES PARA SECCIONES AUN NO HECHAS DE LA PAGINA
+function funcionAlertaSeccionBloqueada() {
+    const variableSeccionBloqueada = "Disculpa, esta seccion de la pagina aun no esta terminada, por lo tanto aun no puedes acceder a ella.";
+    alert(variableSeccionBloqueada);
+}
+
+// FUNCIONES PARA CUANDO ALGUIEN SE REGISTRE
+function funcionAlertaRegistro() {
+    const variableRegistro = "Perfecto!. Ya estas registrado, ahora te invitamos a que veas otras seccion de nuestra pagina para que nos conozcas mejor!";
+    alert(variableRegistro);
+}
+
+// FUNCION PARA REDES SOCIALES QUE NO EXISTEN
+function funcionAlertaRedesSocialesFalsas() {
+    const variableRedesSocialesFalsas= "Disculpa, por ahora no tenemos estas redes activadas. En cuanto las activamos las tendras disponibles!";
+    alert(variableRedesSocialesFalsas);
+}
